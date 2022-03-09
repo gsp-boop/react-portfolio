b8df4f5dbfc7f1411ef34e2c1897c67266ef4145
Gabe Pereira <omami.thatsgood@gmail.com>
2022-03-08 22:36:12 -0500

    updating text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { ABOUTME } from "./utils/content";
-import { FaArrowAltCircleRight } from "react-icons/fa";
+import { FaArrowAltCircleRight, FaSmile } from "react-icons/fa";
 import "../style/about.css";
 
 const About = () => {
@@ -20,31 +20,30 @@ const About = () => {
         <h2 className="display-2 my_name">Gabe Who?</h2>
         <div className="about_content">
           <div className="caraousel">
-              {
-                ABOUTME.map((item, i) => {
-                  if( count === i) {
-                    return(
-                    <div key={item.id}>
-                      <div className={item.class + " pic_active"} >
-                        <img src={item.img} alt="" />
-                      </div>
-                      <p className="subHeading">{item.subHeading}</p>
+            {ABOUTME.map((item, i) => {
+              if (count === i) {
+                return (
+                  <div key={item.id}>
+                    <div className={item.class + " pic_active"}>
+                      <img src={item.img} alt="" />
                     </div>
-                    )
-
-                  }
-                })
+                    <p className="subHeading">{item.subHeading}</p>
+                  </div>
+                );
               }
-              <div className="arrow">
-                <FaArrowAltCircleRight onClick={changeImg}/>
-              </div>
+            })}
+            <div className="arrow">
+              <FaArrowAltCircleRight onClick={changeImg} />
+            </div>
           </div>
           <p>
-            Hello! Make it look good and make it work. I love taking a design to
-            life and work towards making a project look simple and effortless.{" "}
-            <br></br> I like beers, particular a crushable Pils and will spend a
-            concerning amount of time looking for the right bourbon. Food,
-            drinks and tech. Name a better trio.
+            Hello! Make it look good and make it work. I love taking a design
+            and bringing it to life. My focus is frontend using technologies
+            like React.js and Bootstrap.
+            <br></br>
+            <br></br> In my free time I enjoy cooking in the kitchen, hiking for
+            the views and bourbon picking.
+            <FaSmile />
           </p>
         </div>
       </div>
